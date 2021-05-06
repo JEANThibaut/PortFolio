9471c980f098c7a1064f3c289d396997b49a0178
Thibaut <thibaut-jean@live.fr>
2021-05-06 19:22:22 +0200

    add json fonction to make card

--- src/components/layout/nav/Nav.js
+++ src/components/layout/nav/Nav.js
@@ -4,14 +4,14 @@ import {Link} from "react-router-dom";
 function Nav(){
     return (
      
-            <div className="container text-center bg-dark ">
-                <div className="card-body">
+            <div className="row text-center justify-content-around bg-dark ">
+                <div className=" col-lg-2 my-2">
                     <Link className="nav-link" to="/projects"><h3>Mes Projets</h3></Link>
                 </div>
-                <div className="card-body">
+                <div className=" col-lg-2 my-2">
                     <Link className="nav-link" to="/biographie"><h3>Biographie</h3></Link>
                 </div>
-                <div className="card-body bg-warning">
+                <div className=" bg-warning col-lg-2 my-2">
                     <Link className="nav-link" to="/contact"><h3>Contact</h3></Link>
                 </div>
             </div>  
--- src/components/projects/projectscards/ProjectsCards.js
+++ src/components/projects/projectscards/ProjectsCards.js
@@ -7,23 +7,51 @@ import {
   } from 'reactstrap';
 
 
-function ProjectsCards(props){     
+
+function ProjectsCards(){     
+
+
+    return(   <div>
+        {jsonprojects.map((project)=>{ 
+            let projectImage = JSON.stringify(project.image)
+            console.log(projectImage)
+            return (
+                <div>
+                    <Card  style={{ width: '18rem' }}>
+                        <CardBody>
+                           
+                            <CardTitle>{project.title}</CardTitle>
+                            <CardSubtitle className="mb-2 ">{project.project}</CardSubtitle>
+                            <CardText>
+                                {project.description}
+                            </CardText>
+                            <div className="container d-flex justify-content-around">
+                                <div><a href="#">{project.giturl}</a></div>
+                                <div><a href="#"><div>{project.ghpage}</div></a></div>
+                            </div>
+                        </CardBody>
+                    </Card> 
  
-    return(   
-        <Card  style={{ width: '18rem' }}>
-            <CardBody>
-                <CardTitle>Titre</CardTitle>
-                <CardSubtitle className="mb-2 ">Type du projet</CardSubtitle>
-                <CardText>
-                        Description
-                </CardText>
-                <div className=" container d-flex justify-content-around">
-                    <div><a href="#">Lien1</a></div>
-                    <div><a href="#">Lien2</a></div>
                 </div>
-            </CardBody>
-        </Card>
+            )
+        })}
+    </div>
+
     )
 }
             
 export default ProjectsCards;
+
+<Card  style={{ width: '18rem' }}>
+<CardBody>
+    <CardTitle>Titre</CardTitle>
+    <CardSubtitle className="mb-2 ">Type du projet</CardSubtitle>
+    <CardText>
+            Description
+    </CardText>
+    <div className=" container d-flex justify-content-around">
+        <div><a href="#">Lien1</a></div>
+        <div><a href="#">Lien2</a></div>
+    </div>
+</CardBody>
+</Card> 
