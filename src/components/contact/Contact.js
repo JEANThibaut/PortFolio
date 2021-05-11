0b08c7428f2140efa822d58ad1776207652cd835
Thibaut <thibaut-jean@live.fr>
2021-05-11 15:26:39 +0200

    add all content except bio

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,18 @@
 function Contact(){
     return(   
         <div>
-        <a href="mailto:thibaut-jean@live.fr">
-            <img className= "contactLogo" src="img/imgLogo/enveloppe.png"></img>
-        </a>
+            <div>
+                <a href="mailto:thibaut-jean@live.fr">
+                    <img className= "contactLogo" src="img/imgLogo/enveloppe.png"></img>
+                </a>
+            </div>
+            <div className="py-3 my-5 text-center bio">
+                <p>Afin de communiquer merci de me faire parvenir un mail en cliquant sur l'enveloppe en précisant bien l'objet de votre demande!</p>
+            </div>
         </div>
+        
+
+        
     )
 }
 export default Contact;
